806161725729743a029e32c0d671cf7479872bed
Vishwa7787 <vishwagmath@gmail.com>
2024-07-08 07:41:49 +0530

    for checking

---
+++ Add_Sub_Mul_Divide_Mod.java
@@ -0,0 +1,62 @@
+package learning.programs;
+
+public class Add_Sub_Mul_Divide_Mod 
+{
+	 static void add()
+	 {
+		 int no1=10;
+	     int no2=20;
+	     int sum=no1+no2;
+	     
+	     System.out.println(sum);
+	 }
+	 
+	 static void sub()
+	 {
+		 int no1=10;
+	     int no2=20;
+		 int subtraction=no2-no1;
+		 
+		 System.out.println(subtraction);
+	 }
+	 
+	 static void mul()
+	 {
+		 int no1=10;
+		 int no2=20;
+		 int multiplication=no1*no2;
+		  
+		 System.out.println(multiplication);
+		 
+	 }
+	
+	static void div()
+	{
+		int no1=10;
+		int no2=20;
+		int division=no2/no1;
+		
+		System.out.println(division);
+		
+	}
+	
+     static void mod()
+     {
+    	 int no1=10;
+    	 int no2=20;
+    	 int modulus=no2%no1;
+    	 
+    	 System.out.println(modulus);
+    	 
+    	 
+     }
+     public static void main(String[] args)
+     {
+    	 add();
+    	 sub();
+    	 mul();
+    	 div();
+    	 mod();
+    	 
+	}
+}
